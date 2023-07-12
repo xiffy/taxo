9933b43bbbf91b7dfcfce529b10f14b804bbde52
Jochem Oosterlee <jochem.oosterlee@cleverbase.com>
2023-07-12 11:08:42 +0200

    update

---
+++ test-package.bat
@@ -0,0 +1,6 @@
+@echo off
+del .\jenv_taxonomy_2022.zip
+del .\rj_taxonomy_2022.zip
+"C:\Program Files\7-Zip\7z.exe" a .\jenv_taxonomy_2022.zip .\jenv_taxonomy_2022\ -aoa -tzip -bb3 > 7zip-log.txt
+"C:\Program Files\7-Zip\7z.exe" a .\rj_taxonomy_2022.zip .\rj_taxonomy_2022\ -aoa -tzip -bb3 >> 7zip-log.txt
+"C:\Program Files\Arelle\arelleCmdLine.exe" --packages ".\rj_taxonomy_2022.zip|.\jenv_taxonomy_2022.zip" -f https://www.rjnet.nl/taxonomy/2023-06-30/rj_cor.xsd -v
