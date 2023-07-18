d200396f8666e4b4f46b77035ef6fd0e596c7439
Jochem Oosterlee <jochem.oosterlee@cleverbase.com>
2023-07-18 13:48:00 +0200

    instance validation

--- test-instance.bat
+++ test-instance.bat
@@ -11,5 +11,5 @@ del .\7zip-log.txt
 "C:\Program Files\7-Zip\7z.exe" a .\jenv_taxonomy_2022.zip .\jenv_taxonomy_2022\ -aoa -tzip -bb3 >> 7zip-log.txt
 "C:\Program Files\7-Zip\7z.exe" a .\rj_taxonomy_2022.zip .\rj_taxonomy_2022\ -aoa -tzip -bb3 >> 7zip-log.txt
 "C:\Program Files\7-Zip\7z.exe" a .\currency-PWD-2023-06-21.zip .\currency-PWD-2023-06-21\ -aoa -tzip -bb3 >> 7zip-log.txt
-"C:\Program Files\Arelle\arelleCmdLine.exe" --packages ".\rj_taxonomy_2022.zip|.\jenv_taxonomy_2022.zip|.\nen_taxonomy_2022.zip|.\iso_taxonomy_2022.zip|.\currency-PWD-2023-06-21.zip" --file "https://www.rjnet.nl/taxonomy/2023-06-30/rj_cor.xsd|https://www.nen.nl/taxonomy/2023-06-30/nen_1888_2002.xsd|https://www.nen.nl/taxonomy/2023-06-30/nen_5825_2002.xsd|https://www.iso.org/taxonomy/2023-06-30/iso_19160-4_2023.xsd" -v
+"C:\Program Files\Arelle\arelleCmdLine.exe" --packages ".\rj_taxonomy_2022.zip|.\jenv_taxonomy_2022.zip|.\nen_taxonomy_2022.zip|.\iso_taxonomy_2022.zip|.\currency-PWD-2023-06-21.zip" --file "test.xbrl" -v
 @REM --logFile arelle-log.xml
