dc2239f3b256f16046838713833753a6f2aef1ab
Jochem Oosterlee <jochem.oosterlee@cleverbase.com>
2023-07-18 11:26:45 +0200

    added nen schemas

--- test-package.bat
+++ test-package.bat
@@ -1,8 +1,10 @@
 @echo off
+del .\nen_taxonomy_2022.zip
 del .\jenv_taxonomy_2022.zip
 del .\rj_taxonomy_2022.zip
 del .\arelle-log.xml
 del .\7zip-log.txt
-"C:\Program Files\7-Zip\7z.exe" a .\jenv_taxonomy_2022.zip .\jenv_taxonomy_2022\ -aoa -tzip -bb3 > 7zip-log.txt
+"C:\Program Files\7-Zip\7z.exe" a .\nen_taxonomy_2022.zip .\nen_taxonomy_2022\ -aoa -tzip -bb3 > 7zip-log.txt
+"C:\Program Files\7-Zip\7z.exe" a .\jenv_taxonomy_2022.zip .\jenv_taxonomy_2022\ -aoa -tzip -bb3 >> 7zip-log.txt
 "C:\Program Files\7-Zip\7z.exe" a .\rj_taxonomy_2022.zip .\rj_taxonomy_2022\ -aoa -tzip -bb3 >> 7zip-log.txt
-"C:\Program Files\Arelle\arelleCmdLine.exe" --packages ".\rj_taxonomy_2022.zip|.\jenv_taxonomy_2022.zip" -f https://www.rjnet.nl/taxonomy/2023-06-30/rj_cor.xsd -v --logFile arelle-log.xml
+"C:\Program Files\Arelle\arelleCmdLine.exe" --packages ".\rj_taxonomy_2022.zip|.\jenv_taxonomy_2022.zip|.\nen_taxonomy_2022.zip" --file "https://www.rjnet.nl/taxonomy/2023-06-30/rj_cor.xsd|https://www.nen.nl/taxonomy/2023-06-30/nen_1888_2002.xsd|https://www.nen.nl/taxonomy/2023-06-30/nen_5825_2002.xsd" -v --logFile arelle-log.xml
