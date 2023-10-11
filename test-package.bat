42d83a1c5758ffe49e51c9194dcfac9f1f938236
Jochem Oosterlee <jochem.oosterlee@cleverbase.com>
2023-10-11 11:32:46 +0200

    references

--- test-package.bat
+++ test-package.bat
@@ -11,5 +11,5 @@ del .\7zip-log.txt
 "C:\Program Files\7-Zip\7z.exe" a .\jenv_taxonomy_2024.zip .\jenv_taxonomy_2024\ -aoa -tzip -bb3 >> 7zip-log.txt
 "C:\Program Files\7-Zip\7z.exe" a .\rj_taxonomy_2024.zip .\rj_taxonomy_2024\ -aoa -tzip -bb3 >> 7zip-log.txt
 "C:\Program Files\7-Zip\7z.exe" a .\currency-CR-2023-08-23.zip .\currency-CR-2023-08-23\ -aoa -tzip -bb3 >> 7zip-log.txt
-".\script\arelle\arelleCmdLine.exe" --packages ".\rj_taxonomy_2024.zip|.\jenv_taxonomy_2024.zip|.\nen_taxonomy_2024.zip|.\iso_taxonomy_2024.zip|.\currency-CR-2023-08-23.zip" --file "https://www.rjnet.nl/taxonomy/2025-01-01/rj_cor.xsd|https://www.nen.nl/taxonomy/2025-01-01/nen_1888_2002.xsd|https://www.nen.nl/taxonomy/2025-01-01/nen_5825_2002.xsd|https://www.iso.org/taxonomy/2025-01-01/iso_19160-4_2023.xsd" -v
+".\script\arelle\arelleCmdLine.exe" --packages ".\rj_taxonomy_2024.zip|.\jenv_taxonomy_2024.zip|.\nen_taxonomy_2024.zip|.\iso_taxonomy_2024.zip|.\currency-CR-2023-08-23.zip" --file "https://www.minjenv.nl/taxonomy/2025-01-01/jenv-bw2_cor.xsd|https://www.rjnet.nl/taxonomy/2025-01-01/rj_cor.xsd|https://www.nen.nl/taxonomy/2025-01-01/nen_1888_2002.xsd|https://www.nen.nl/taxonomy/2025-01-01/nen_5825_2002.xsd|https://www.iso.org/taxonomy/2025-01-01/iso_19160-4_2023.xsd" -v
 @REM --logFile arelle-log.xml
