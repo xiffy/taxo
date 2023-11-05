68e77e42ed99b5a987d602c89503fad8e865feee
Appie Verschoor <appie@cooljapan.nl>
2023-11-05 01:30:37 +0100

    local versuild of taxonomy-build.yml

--- test-package.bat
+++ test-package.bat
@@ -1,14 +1,18 @@
 @echo off
 setlocal enableDelayedExpansion
-#Building taxonomy-packages
-call ./script/cicd/make-artifacts.bat
-echo Creating package list
-call ./script/cicd/make-packagelist.bat
-echo setting temp
-set /p TMP_LIST=<artifacts/package_list.env
-echo setting list
+echo Building taxonomy-packages
+call .\script\cicd\make-artifacts.bat
 
+echo Creating package list
+call .\script\cicd\make-packagelist.bat
+
+echo setting temp
+set /p TMP_LIST=<artifacts\package_list.env
+
+echo setting list
 set "LIST=%TMP_LIST:~13%"
-echo "./script/arelle/arelleCmdline.exe -v --packages '!LIST!' > huh.txt"
+
 echo calling Arelle
-cmd /c "./script/arelle/arelleCmdline.exe -v --packages '!LIST!' --file 'https://www.rjnet.nl/taxonomy/2025-01-01/rj_cor.xsd|https://www.nen.nl/taxonomy/2025-01-01/nen_1888_2002.xsd|https://www.nen.nl/taxonomy/2025-01-01/nen_5825_2002.xsd|https://www.iso.org/taxonomy/2025-01-01/iso_19160-4_2023.xsd' -v"
+.\script\arelle\arelleCmdLine.exe -v --packages "!LIST!" --file "https://www.rjnet.nl/taxonomy/2025-01-01/rj_cor.xsd|https://www.nen.nl/taxonomy/2025-01-01/nen_1888_2002.xsd|https://www.nen.nl/taxonomy/2025-01-01/nen_5825_2002.xsd|https://www.iso.org/taxonomy/2025-01-01/iso_19160-4_2023.xsd" -v
+
+echo terug
