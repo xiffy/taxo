530a20722724876401782c7a95da9ed94ae5e810
Appie Verschoor <appie@cooljapan.nl>
2023-11-06 15:34:33 +0100

    and thus you test all instances.

--- test-instance.bat
+++ test-instance.bat
@@ -3,8 +3,14 @@ setlocal enableDelayedExpansion
 echo Building taxonomy-packages
 call .\script\cicd\make-artifacts.bat
 set /p TMP_LIST=<artifacts\package_list.env
+@REM remove PACKAGE_LIST= from the string (github needs that)
 set "LIST=%TMP_LIST:~13%"
+echo Gathering test instances
+call .\script\cicd\make-instancelist.bat
+set /p TMP_LIST=<instances\instance_list.env
+@REM remove INSTANCE_LIST= from the string (github needs that)
+set "INSTANCE_LIST=%TMP_LIST:~14%"
 
 echo testing instance
-"C:\Program Files\Arelle\arelleCmdLine.exe" --packages "!LIST!" --file "instances/test.xbrl" -v
+"C:\Program Files\Arelle\arelleCmdLine.exe" --packages "!LIST!" --file "!INSTANCE_LIST!" -v
 @REM --logFile arelle-log.xml
