8bbd302050b3f1cb1958d111ee74012e3b8d5428
Appie Verschoor <appie@cooljapan.nl>
2023-11-06 15:42:53 +0100

    typo in cicd en geef lijsten duidelijke naam.

--- test-instance.bat
+++ test-instance.bat
@@ -4,7 +4,7 @@ echo Building taxonomy-packages
 call .\script\cicd\make-artifacts.bat
 set /p TMP_LIST=<artifacts\package_list.env
 @REM remove PACKAGE_LIST= from the string (github needs that)
-set "LIST=%TMP_LIST:~13%"
+set "PACKAGE_LIST=%TMP_LIST:~13%"
 echo Gathering test instances
 call .\script\cicd\make-instancelist.bat
 set /p TMP_LIST=<instances\instance_list.env
@@ -12,5 +12,5 @@ set /p TMP_LIST=<instances\instance_list.env
 set "INSTANCE_LIST=%TMP_LIST:~14%"
 
 echo testing instance
-"C:\Program Files\Arelle\arelleCmdLine.exe" --packages "!LIST!" --file "!INSTANCE_LIST!" -v
+"C:\Program Files\Arelle\arelleCmdLine.exe" --packages "!PACKAGE_LIST!" --file "!INSTANCE_LIST!" -v
 @REM --logFile arelle-log.xml
