59f10b229dc34e30ca49da3d0866a5a92318467a
appie verschoor <appie@cooljapan.nl>
2023-11-07 15:37:02 +0100

    in simpele vorm kan ju nu je eigen instance(lijst) doorgeven op de command-prompt

--- test-instance.bat
+++ test-instance.bat
@@ -5,12 +5,17 @@ call .\script\cicd\make-artifacts.bat
 set /p TMP_LIST=<artifacts\package_list.env
 @REM remove PACKAGE_LIST= from the string (github needs that)
 set "PACKAGE_LIST=%TMP_LIST:~13%"
-echo Gathering test instances
-call .\script\cicd\make-instancelist.bat
-set /p TMP_LIST=<instances\instance_list.env
-@REM remove INSTANCE_LIST= from the string (github needs that)
-set "INSTANCE_LIST=%TMP_LIST:~14%"
 
-echo testing instance
+if "%~1"=="" (
+  echo Gathering test instances
+  call .\script\cicd\make-instancelist.bat
+  set /p TMP_LIST=<instances\instance_list.env
+  @REM remove INSTANCE_LIST= from the string (github needs that)
+  set "INSTANCE_LIST=%TMP_LIST:~14%"
+  echo testing all instances
+) else (
+  echo testing instance: %1
+  set "INSTANCE_LIST=%1"
+)
 "C:\Program Files\Arelle\arelleCmdLine.exe" --packages "!PACKAGE_LIST!" --file "!INSTANCE_LIST!" -v
 @REM --logFile arelle-log.xml
