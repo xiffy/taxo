ae3c8a8be81ecba0b23a045d2284a077778ab125
Appie Verschoor <appie@cooljapan.nl>
2023-11-07 11:54:17 +0100

    and that erroneous substring caused all the trouble. Now we're validating local as well as remote

--- test-package.bat
+++ test-package.bat
@@ -11,10 +11,10 @@ set "PACKAGE_LIST=%TMP_LIST:~13%"
 echo Creating entrypoint list and read into ENTRYPOINT_LIST
 call .\script\cicd\make-entrypointlist.bat
 set /p TMP_LIST=<taxonomies\entrypoint_list.env
-set "ENTRYPOINT_LIST=%TMP_LIST:~13%"
+set "ENTRYPOINT_LIST=%TMP_LIST:~16%"
 
 echo calling Arelle
-.\script\arelle\arelleCmdLine.exe -v --packages "!PACKAGE_LIST!" --file="!ENTRYPOINT_LIST!" -v
+.\script\arelle\arelleCmdLine.exe --validate --file="!ENTRYPOINT_LIST!" --packages "!PACKAGE_LIST!"
 rem "https://www.rjnet.nl/taxonomy/2025-01-01/rj_cor.xsd|
 rem https://www.nen.nl/taxonomy/2025-01-01/nen_1888_2002.xsd|
 rem https://www.nen.nl/taxonomy/2025-01-01/nen_5825_2002.xsd|
