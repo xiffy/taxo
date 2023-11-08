1158bba91e8d5ba4bfd0b2693f4bfe805aa8e3f4
appie verschoor <appie@cooljapan.nl>
2023-11-08 15:46:31 +0100

    lognaam gelijk houden, let's see

--- script/cicd/catch_errors.bat
+++ script/cicd/catch_errors.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal enableDelayedExpansion
-for %%f in (arelle.log) do set "SIZE=%%~zf"
+for %%f in (arelle-taxo.log) do set "SIZE=%%~zf"
 if "!SIZE!" NEQ "0" (
-  type arelle.log
+  type arelle-taxo.log
   exit /b 1
 ) else (
   echo "Goed hoor"
--- test-package.bat
+++ test-package.bat
@@ -14,6 +14,6 @@ set /p TMP_LIST=<taxonomies\entrypoint_list.env
 set "ENTRYPOINT_LIST=%TMP_LIST:~16%"
 
 echo calling Arelle
-for /f %%d in ("arelle.log") do del %%d
+for /f %%d in ("arelle-taxo.log") do del %%d
 .\script\arelle\arelleCmdLine.exe --validate --file="!ENTRYPOINT_LIST!" --packages "!PACKAGE_LIST!" --logFile=arelle-taxo.log --logLevel=warning
 type arelle-taxo.log
