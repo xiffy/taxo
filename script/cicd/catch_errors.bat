3903245e710d8cea3bfada173b1db2bcf0d5a8bb
appie verschoor <appie@cooljapan.nl>
2023-11-09 10:22:28 +0100

    En alle referenties naar de logfile parametriseren.

--- script/cicd/catch_errors.bat
+++ script/cicd/catch_errors.bat
@@ -2,7 +2,7 @@
 setlocal enableDelayedExpansion
 for %%f in (%1) do set "SIZE=%%~zf"
 if "!SIZE!" NEQ "0" (
-  type arelle-taxo.log
+  type %1
   exit /b 1
 ) else (
   echo Arelle says: All good! (no warnings or errors^)
