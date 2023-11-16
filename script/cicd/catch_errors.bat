cdd0cf69c55fec5ded4fc827101fc5391302bcab
appie verschoor <appie@cooljapan.nl>
2023-11-16 11:28:41 +0100

    Eens zien of Github stderr leest (ik hoop dat ik naar stderr output) Fout afdwingen door typo in taxo

--- script/cicd/catch_errors.bat
+++ script/cicd/catch_errors.bat
@@ -1,8 +1,10 @@
 @echo off
 setlocal enableDelayedExpansion
 for %%f in (%1) do set "SIZE=%%~zf"
-if "!SIZE!" NEQ "0" (
+if "%SIZE%" NEQ "0" (
   type %1
+  set /p LOG=<%1
+  echo !LOG! >&2
   exit /b 1
 ) else (
   echo Arelle says: All good! (no warnings or errors^)
